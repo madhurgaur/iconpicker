f2d9ddbe8c9ce531ab4ea4a1db97918fac5df875
Madhur Gaur <madhurgaur9140@gmail.com>
2024-06-14 19:19:48 +0530

    cssupd

--- src/components/Iconmodel.jsx
+++ src/components/Iconmodel.jsx
@@ -1,10 +1,130 @@
+// import React, { useState } from 'react';
+// import './IconModel.css';
+
+// const icons = [
+//   '😀', '😃', '😄', '😁', '😆', '😅', '🤣', '😂', '🙂', '🙃', '😉', '😊',  '😀', '😃', '😄', '😁', '😆', '😅', '🤣', '😂', '🙂', '🙃', '😉', '😊',  '😀', '😃', '😄', '😁', '😆', '😅', '🤣', '😂', '🙂', '🙃', '😉', '😊',  '😀', '😃', '😄', '😁', '😆', '😅', '🤣', '😂', '🙂', '🙃', '😉', '😊',  '😀', '😃', '😄', '😁', '😆', '😅', '🤣', '😂', '🙂', '🙃', '😉', '😊',  '😀', '😃', '😄', '😁', '😆', '😅', '🤣', '😂', '🙂', '🙃', '😉', '😊',  '😀', '😃', '😄', '😁', '😆', '😅', '🤣', '😂', '🙂', '🙃', '😉', '😊',  '😀', '😃', '😄', '😁', '😆', '😅', '🤣', '😂', '🙂', '🙃', '😉', '😊',  '😀', '😃', '😄', '😁', '😆', '😅', '🤣', '😂', '🙂', '🙃', '😉', '😊',  '😀', '😃', '😄', '😁', '😆', '😅', '🤣', '😂', '🙂', '🙃', '😉', '😊',  '😀', '😃', '😄', '😁', '😆', '😅', '🤣', '😂', '🙂', '🙃', '😉', '😊',  '😀', '😃', '😄', '😁', '😆', '😅', '🤣', '😂', '🙂', '🙃', '😉', '😊',  '😀', '😃', '😄', '😁', '😆', '😅', '🤣', '😂', '🙂', '🙃', '😉', '😊',  '😀', '😃', '😄', '😁', '😆', '😅', '🤣', '😂', '🙂', '🙃', '😉', '😊',  '😀', '😃', '😄', '😁', '😆', '😅', '🤣', '😂', '🙂', '🙃', '😉', '😊',
+//   // Add more icons as needed
+// ];
+
+// function Iconmodel({
+//   rowsInOnePage,
+//   columnsInOnePage,
+//   iconHeight,
+//   iconWidth,
+//   pickerHeight = '500px',
+//   pickerWidth = '500px',
+//   onIconSelect,import React, { useState } from 'react';
+// import * as Icons from 'react-feather';
+// import './IconModel.css';
+
+// // Extracting the icon names from the react-feather library
+// const iconNames = Object.keys(Icons);
+
+// function Iconmodel({
+//   rowsInOnePage,
+//   columnsInOnePage,
+//   iconHeight,
+//   iconWidth,
+//   pickerHeight = '500px',
+//   pickerWidth = '500px',
+//   onIconSelect,
+//   onClose
+// }) {
+//   const iconsPerPage = rowsInOnePage * columnsInOnePage;
+//   const totalPages = Math.ceil(iconNames.length / iconsPerPage);
+//   const [currentPage, setCurrentPage] = useState(1);
+
+//   const handlePageChange = (newPage) => {
+//     if (newPage > 0 && newPage <= totalPages) {
+//       setCurrentPage(newPage);
+//     }
+//   };
+
+//   const startIndex = (currentPage - 1) * iconsPerPage;
+//   const endIndex = startIndex + iconsPerPage;
+//   const currentIcons = iconNames.slice(startIndex, endIndex);
+
+//   return (
+//     <div className="icon-picker-modal" style={{ height: pickerHeight, width: pickerWidth }}>
+//       <div className="icon-picker-header">
+//         <h3>Select App Icon</h3>
+//         <button onClick={onClose}>X</button>
+//       </div>
+//       <div className="icon-picker-grid" style={{ gridTemplateColumns: `repeat(${columnsInOnePage}, ${iconWidth}px)`, gridTemplateRows: `repeat(${rowsInOnePage}, ${iconHeight}px)` }}>
+//         {currentIcons.map((iconName, index) => {
+//           const IconComponent = Icons[iconName];
+//           return (
+//             <div
+//               key={index}
+//               className="icon-picker-item"
+//               style={{ height: iconHeight, width: iconWidth }}
+//               onClick={() => onIconSelect(iconName)}
+//             >
+//               <IconComponent />
+//             </div>
+//           );
+//         })}
+//       </div>
+//       <div className="icon-picker-footer">
+//         <button onClick={() => handlePageChange(currentPage - 1)} disabled={currentPage === 1}>Previous</button>
+//         <span>Page {currentPage} of {totalPages}</span>
+//         <button onClick={() => handlePageChange(currentPage + 1)} disabled={currentPage === totalPages}>Next</button>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Iconmodel;
+//   onClose
+// }) {
+//   const iconsPerPage = rowsInOnePage * columnsInOnePage;
+//   const totalPages = Math.ceil(icons.length / iconsPerPage);
+//   const [currentPage, setCurrentPage] = useState(1);
+
+//   const handlePageChange = (newPage) => {
+//     if (newPage > 0 && newPage <= totalPages) {
+//       setCurrentPage(newPage);
+//     }
+//   };
+
+//   const startIndex = (currentPage - 1) * iconsPerPage;
+//   const endIndex = startIndex + iconsPerPage;
+//   const currentIcons = icons.slice(startIndex, endIndex);
+
+//   return (
+//     <div className="icon-picker-modal" style={{ height: pickerHeight, width: pickerWidth }}>
+//       <div className="icon-picker-header">
+//         <h3>Select App Icon</h3>
+//         <button onClick={onClose}>X</button>
+//       </div>
+//       <div className="icon-picker-grid">
+//         {currentIcons.map((icon, index) => (
+//           <div
+//             key={index}
+//             className="icon-picker-item"
+//             style={{ height: iconHeight, width: iconWidth }}
+//             onClick={() => onIconSelect(icon)}
+//           >
+//             {icon}
+//           </div>
+//         ))}
+//       </div>
+//       <div className="icon-picker-footer">
+//         <button onClick={() => handlePageChange(currentPage - 1)}>Previous</button>
+//         <span>Page {currentPage} of {totalPages}</span>
+//         <button onClick={() => handlePageChange(currentPage + 1)}>Next</button>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default Iconmodel;
 import React, { useState } from 'react';
+import * as Icons from 'react-feather';
 import './IconModel.css';
 
-const icons = [
-  '😀', '😃', '😄', '😁', '😆', '😅', '🤣', '😂', '🙂', '🙃', '😉', '😊',  '😀', '😃', '😄', '😁', '😆', '😅', '🤣', '😂', '🙂', '🙃', '😉', '😊',  '😀', '😃', '😄', '😁', '😆', '😅', '🤣', '😂', '🙂', '🙃', '😉', '😊',  '😀', '😃', '😄', '😁', '😆', '😅', '🤣', '😂', '🙂', '🙃', '😉', '😊',  '😀', '😃', '😄', '😁', '😆', '😅', '🤣', '😂', '🙂', '🙃', '😉', '😊',  '😀', '😃', '😄', '😁', '😆', '😅', '🤣', '😂', '🙂', '🙃', '😉', '😊',  '😀', '😃', '😄', '😁', '😆', '😅', '🤣', '😂', '🙂', '🙃', '😉', '😊',  '😀', '😃', '😄', '😁', '😆', '😅', '🤣', '😂', '🙂', '🙃', '😉', '😊',  '😀', '😃', '😄', '😁', '😆', '😅', '🤣', '😂', '🙂', '🙃', '😉', '😊',  '😀', '😃', '😄', '😁', '😆', '😅', '🤣', '😂', '🙂', '🙃', '😉', '😊',  '😀', '😃', '😄', '😁', '😆', '😅', '🤣', '😂', '🙂', '🙃', '😉', '😊',  '😀', '😃', '😄', '😁', '😆', '😅', '🤣', '😂', '🙂', '🙃', '😉', '😊',  '😀', '😃', '😄', '😁', '😆', '😅', '🤣', '😂', '🙂', '🙃', '😉', '😊',  '😀', '😃', '😄', '😁', '😆', '😅', '🤣', '😂', '🙂', '🙃', '😉', '😊',  '😀', '😃', '😄', '😁', '😆', '😅', '🤣', '😂', '🙂', '🙃', '😉', '😊',
-  // Add more icons as needed
-];
+// Extracting the icon names from the react-feather library
+const iconNames = Object.keys(Icons);
 
 function Iconmodel({
   rowsInOnePage,
@@ -17,7 +137,7 @@ function Iconmodel({
   onClose
 }) {
   const iconsPerPage = rowsInOnePage * columnsInOnePage;
-  const totalPages = Math.ceil(icons.length / iconsPerPage);
+  const totalPages = Math.ceil(iconNames.length / iconsPerPage);
   const [currentPage, setCurrentPage] = useState(1);
 
   const handlePageChange = (newPage) => {
@@ -28,7 +148,7 @@ function Iconmodel({
 
   const startIndex = (currentPage - 1) * iconsPerPage;
   const endIndex = startIndex + iconsPerPage;
-  const currentIcons = icons.slice(startIndex, endIndex);
+  const currentIcons = iconNames.slice(startIndex, endIndex);
 
   return (
     <div className="icon-picker-modal" style={{ height: pickerHeight, width: pickerWidth }}>
@@ -36,22 +156,25 @@ function Iconmodel({
         <h3>Select App Icon</h3>
         <button onClick={onClose}>X</button>
       </div>
-      <div className="icon-picker-grid">
-        {currentIcons.map((icon, index) => (
-          <div
-            key={index}
-            className="icon-picker-item"
-            style={{ height: iconHeight, width: iconWidth }}
-            onClick={() => onIconSelect(icon)}
-          >
-            {icon}
-          </div>
-        ))}
+      <div className="icon-picker-grid" style={{ gridTemplateColumns: `repeat(${columnsInOnePage}, ${iconWidth}px)`, gridTemplateRows: `repeat(${rowsInOnePage}, ${iconHeight}px)` }}>
+        {currentIcons.map((iconName, index) => {
+          const IconComponent = Icons[iconName];
+          return (
+            <div
+              key={index}
+              className="icon-picker-item"
+              style={{ height: iconHeight, width: iconWidth }}
+              onClick={() => onIconSelect(iconName)}
+            >
+              <IconComponent />
+            </div>
+          );
+        })}
       </div>
       <div className="icon-picker-footer">
-        <button onClick={() => handlePageChange(currentPage - 1)}>Previous</button>
+        <button onClick={() => handlePageChange(currentPage - 1)} disabled={currentPage === 1}>Previous</button>
         <span>Page {currentPage} of {totalPages}</span>
-        <button onClick={() => handlePageChange(currentPage + 1)}>Next</button>
+        <button onClick={() => handlePageChange(currentPage + 1)} disabled={currentPage === totalPages}>Next</button>
       </div>
     </div>
   );
